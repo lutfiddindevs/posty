482b1b3fdaa9442b4aabfc4b994cf8e7094372cb
lutfiddin <shamsidinovlutfiddin@gmail.com>
2021-03-24 22:18:32 +0500

    register controller, store and validating user

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+    public function index() {
+    	return view('auth.register');
+    }
+
+    public function store(Request $req) {
+    	$this->validate($req, [
+    		'name' => 'required|max:255',
+    		'username' => 'required|max:255',
+    		'email' => 'required|email|max:255',
+    		'password' => 'required|confirmed',
+    	]);
+
+    	User::create([
+    		'name' => $req->name,
+    		'username' => $req->username,
+    		'email' => $req->email,
+    		'password' => Hash::make($req->password)
+    	]);
+
+    	return redirect()->route('dashboard');
+
+    }
+}
