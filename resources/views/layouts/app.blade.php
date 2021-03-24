4794407892ee1d1a23ff3e336721e9ceff8ef1be
lutfiddin <shamsidinovlutfiddin@gmail.com>
2021-03-24 17:38:14 +0500

    styling the layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>posty</title>
+	<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body class="bg-gray-200">
+    <nav class="p-6 bg-white flex justify-between mb-6">
+    	<ul class="flex items-center">
+    		<li>
+    			<a href="" class="p-3">home</a>
+    		</li>
+    		<li>
+    			<a href="" class="p-3">dashboard</a>
+    		</li>
+    		<li>
+    			<a href="" class="p-3">post</a>
+    		</li>
+    	</ul>
+    	<ul class="flex items-center">
+    		<li>
+    			<a href="" class="p-3">Alex Smith</a>
+    		</li>
+    		<li>
+    			<a href="" class="p-3">login</a>
+    		</li>
+    		<li>
+    			<a href="" class="p-3">register</a>
+    		</li>
+    		<li>
+    			<a href="" class="p-3">logout</a>
+    		</li>
+    	</ul>
+    </nav>
+	@yield('content')
+</body>
+</html>
