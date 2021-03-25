53c4a20388d6a2d71d2ed52fcca7501d53219faf
lutfiddin <shamsidinovlutfiddin@gmail.com>
2021-03-25 11:03:33 +0500

    remember input

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -20,6 +20,12 @@
   				<input type="password" name="password" id="password" placeholder="Choose a password" class="bg-gray-100 border-2 w-full p-4 rounded-lg  @error('password') border-red-500  @enderror">
           @error('password') <div class="text-red-500 mt-2 text-sm">{{ $message }}</div> @enderror
   			</div>
+        <div class="mb-4">
+          <div class="flex items-center">
+            <input type="checkbox" name="remember" id="remember" class="mr-2">
+            <label for="remember">Remember me</label>  
+          </div> 
+        </div>
   			<div>
   				<button type="submit" class="bg-blue-500 text-white px-4 py-3 rounded font-medium w-full">Login</button>
   			</div>
