ad697d99ce6736efaee86044b14c4e7340346b65
lutfiddin <shamsidinovlutfiddin@gmail.com>
2021-03-26 22:55:37 +0500

    sending emails for liked posts

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,10 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Mail\PostLiked;
+use Illuminate\Support\Facades\Mail;
 
 class DashboardController extends Controller
 {
+    public function __construct() {
+    	$this->middleware(['auth']);
+    }
+
     public function index() {
+    	$user = auth()->user();
+    	Mail::to($user)->send(new PostLiked());
     	return view('dashboard');
     }
+
 }
