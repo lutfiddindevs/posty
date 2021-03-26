6bb06b6714bacc847d6f111124532155d6b4a682
lutfiddin <shamsidinovlutfiddin@gmail.com>
2021-03-26 18:09:01 +0500

    added show method

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,6 +12,12 @@ class PostController extends Controller
         $posts = Post::latest()->with(['user', 'likes'])->paginate(20);
     	return view('posts.index', ['posts' => $posts]);
     }
+    
+    public function show(Post $post) {
+        return view('posts.show', [
+            'post' => $post
+        ]);
+    }
 
     public function store(Request $req) {
     	$this->validate($req, [
