dfe7520dd797d90c803a4862a24278f3c3ea41e2
lutfiddin <shamsidinovlutfiddin@gmail.com>
2021-03-26 18:08:24 +0500

    counting recieved likes

--- app/Models/User.php
+++ app/Models/User.php
@@ -50,4 +50,8 @@ class User extends Authenticatable
     public function likes() {
         return $this->hasMany(Like::class);
     }
+
+    public function recievedLikes() {
+        return $this->hasManyThrough(Like::class, Post::class);
+    }
 }
